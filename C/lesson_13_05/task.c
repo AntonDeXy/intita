dbb166f3d7a8a80ae02fe177b23cadc0973d2382
AntonDeXy <podolyananton@gmail.com>
2019-05-17 22:04:47 +0300

    upd

---
+++ C/lesson_13_05/task.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+main()
+{
+	char word[30], words[100][30];
+	int i = 0;
+
+	FILE *fp;
+	fp = fopen("text.txt", "r");
+
+	while (1){
+		word[0] = 0;
+		fscanf(fp, "%s", word);
+		if (word[0] == 0)
+			break;
+		if ((word[0]>= 'A') && (word[0]<= 'Z') ){
+			strcpy(words[i], word);
+			i++;
+		}
+	}
+	fclose(fp);
+	fp = fopen("copy.txt", "w");
+	i--;
+	while(i>=0){
+		fprintf(fp, "%s\n",words[i]);
+		i--;
+	}
+	fclose(fp);
+	printf("Complete");
+}
+
---
+++ C/lesson_13_05/task.cpp
@@ -0,0 +1,14 @@
+#include <stdion.h>
+
+main(){
+	char word[30], words[100][30];
+	int i=0;
+	
+	
+	FILE *fp;
+	fp = fopen("text.txt", "r");
+	
+	
+//	copy_str = fopen("copy_str.txt", "r");
+}
+
