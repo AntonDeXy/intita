f1a2d7a5612c1ba6993f6e0131b7cb8d17da47f6
AntonDeXy <podolyananton@gmail.com>
2019-06-10 16:27:17 +0300

    upd

--- C/lesson_03_06/int_float.c
+++ C/lesson_03_06/int_float.c
@@ -4,8 +4,9 @@ main()
 {
 	FILE *fp,*fp1,*fp2;
 	char num[20]={};
-	float sumFloat = 0,tempNum, floatToInt[20], sumFloatToInt=0;
-	int n,i, m=0,p=0,point, j,sumInt = 0;
+	float sumFloat = 0,tempNum, floatToInt[20], sumFloatToInt=0, numsForSortFloats[20], numsForSortInt[20];
+	int point, sumInt = 0, intsNum, temp, size=10;
+	int n,i,m=0,p=0,j, k,l,e=0,h=0,kilDrob=0, kilInt=0;
 	
 	fp=fopen("input.txt","r");
 	fp1=fopen("int.txt","w");
@@ -30,36 +31,93 @@ main()
 		
 		if(num[i]==0){
 			if(point==1){
-				fprintf(fp2,"%s ",num);
+//				fprintf(fp2,"%s ",num);
 				sscanf(num,"%f ",&tempNum);
-//				printf("\n---------- %.0f -------\n",tempNum);
-				while(m<20){
+				while(1){
+					numsForSortFloats[e] = tempNum;
+					printf("%g",numsForSortFloats[e]);
+					e++;
+					kilDrob++;
+					break;
+				}
+				while(m<size){
 					floatToInt[m] = tempNum;
-//					printf("%g\n ",tempNum);
 					m++;
 					break;
 				}
-//				printf("%f ",floatToInt)
 				sumFloat=sumFloat+tempNum;
-//				printf("%i ",floatToInt);
 			}
 			else {
-				fprintf(fp1,"%s ",num);
+//				fprintf(fp1,"%s ",num);
 				sscanf(num,"%f",&tempNum);
+				while(1){
+					numsForSortInt[h]=tempNum;
+					h++;
+					kilInt++;
+					break;
+				}
+//				printf("1");
 				sumInt=sumInt+tempNum;
+//				printf("------------ %i",sumInt);
 			}
 		}
-		
 	}
+//	printf("\n%i-------",kilDrob);
+	
+	
+		for (k = 0; k < kilInt - 1; k++) {
+	        for (l= 0; l < kilInt - k - 1 ; l++) {
+	            if (numsForSortInt[l] > numsForSortInt[l + 1]) {
+	                temp = numsForSortInt[l];
+	                numsForSortInt[l] = numsForSortInt[l + 1];
+	                numsForSortInt[l + 1] = temp;
+	            }
+	        }
+	    }
+	    
+	    for (k = 0; k < kilDrob - 1; k++) {
+	        for (l= 0; l < kilDrob - k - 1 ; l++) {
+	            if (numsForSortFloats[l] > numsForSortFloats[l + 1]) {
+	                temp = numsForSortFloats[l];
+	                numsForSortFloats[l] = numsForSortFloats[l + 1];
+	                numsForSortFloats[l + 1] = temp;
+	            }
+	        }
+	    }
+	    
+//		IntFile
+		for(h=0;h<kilInt;h++){
+			fprintf(fp1,"%0.f ",numsForSortInt[h]);
+		}
+		fprintf(fp1,"\nSum = %i", sumInt);
+//		floatFile
+		for(h=0;h<kilDrob;h++){
+			fprintf(fp2,"%.1f ",numsForSortFloats[h]);
+		}
 		fprintf(fp2,"\nSum = %g", sumFloat);
 		fprintf(fp2,"\nCili chysla:  ");
-		fprintf(fp1,"\nSum = %i", sumInt);
-		for(p=0;p<20;p++){
-			if(floatToInt[p] == 0){
-				break;
-			}
-			fprintf(fp2,"%.0f ",floatToInt[p]);
-			sumFloatToInt=sumFloatToInt+floatToInt[p];
+	
+		
+		for (k = 0; k < kilDrob - 1; k++) {
+	        for (l= 0; l < kilDrob - k - 1 ; l++) {
+	            if (floatToInt[l] > floatToInt[l + 1]) {
+	                temp = floatToInt[l];
+	                floatToInt[l] = floatToInt[l + 1];
+	                floatToInt[l + 1] = temp;
+	            }
+	        }
+	    }
+//		
+//		for(i=0;i<kilDrob;i++)
+//			printf("\n%g",floatToInt[i]);
+		
+		for(p=0;p<kilDrob;p++){
+//			if(floatToInt[p] == 0){
+//				break;
+//			}
+			intsNum=floatToInt[p];
+			fprintf(fp2,"%i ",intsNum);
+			sumFloatToInt=sumFloatToInt+intsNum;
 		}
 		fprintf(fp2,"\nSum floatsToInt = %0.f ",sumFloatToInt);
 		fclose(fp);
---
+++ C/lesson_10_06/task.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+struct good{
+	char name[50];
+	long serial;
+	float price;
+	int count;
+};
+
+main(){
+	tovar laptops[50]
+}
